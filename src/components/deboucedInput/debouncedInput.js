d9f8617a025137362a6b5a06c6cdd91ed131904b
Mehdi Raheemun <mraheemu@student.42.fr>
2023-10-26 11:25:48 +0200

    create DebouncedInput

---
+++ src/components/deboucedInput/debouncedInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function DebouncedInput(props) {
+  const {
+    value: initialValue,
+    onChange,
+    debounce = 500,
+    ...otherProps
+  } = props;
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+  }, [value, onChange, debounce]);
+
+  return (
+    <input
+      {...otherProps}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}
+
+export default DebouncedInput;
